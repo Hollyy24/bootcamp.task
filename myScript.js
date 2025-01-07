840e6b841cffd426465c18b108f0b4ad418df20d
Holly <jk699411@gmail.com>
2025-01-07 12:39:06 +0800

    first commit

---
+++ myScript.js
@@ -0,0 +1,15 @@
+
+var burgerButton = document.querySelector(".nav_button");
+var closeButton = document.querySelector(".close_nav");
+var menu = document.querySelector(".menu");
+
+burgerButton.addEventListener('click',function(){
+    menu.classList.add("is_active");
+    closeButton.classList.add("is_active");
+})
+
+
+closeButton.addEventListener('click',function(){
+    menu.classList.remove("is_active");
+    closeButton.classList.remove("is_active");
+})
